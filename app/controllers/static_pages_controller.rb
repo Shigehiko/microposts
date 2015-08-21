367186ed5a2dc9b490cc63119a646343c47c9a66
shigehiko <shige3799@gmail.com>
2015-08-21 12:20:58 +0000

    Gemfile update for heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ def home
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-      @users = current_user.microposts.page(params[:page]).per(5).order(created_at: :desc)
+     
     end
   end
 end
